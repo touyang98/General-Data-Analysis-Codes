8da19a4ebb549db2ca0919432dbe05fb64494180
Tia Ouyang <touyang@umces.edu>
2026-02-26 09:02:55 -0500

    02262026 Updates

--- Aqualog_PARAFAC/Abs_Analysis_Functions.R
+++ Aqualog_PARAFAC/Abs_Analysis_Functions.R
@@ -2,10 +2,14 @@ read_abs <- function(path_abs){
   
   ##read the data files from a folder 
   file_abs <- list.files(path_abs,  pattern = "\\.dat$", full.names = TRUE)
-data_abs <- lapply(file_abs, read.table, header = TRUE)
+data_abs <- lapply(file_abs, read.table, header = FALSE)
 
 ##name the data files 
 names(data_abs) <- sub("\\.dat$", "", basename(file_abs))
+
+for (i in 1:length(data_abs)){
+  colnames(data_abs[[i]]) <- c("wavelength", "abs")
+}
 
 return(data_abs)
 
