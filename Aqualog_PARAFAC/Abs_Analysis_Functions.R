417c618a1c73fb882c64052891b07623a24d57b9
Tia Ouyang <touyang@umces.edu>
2026-02-26 09:31:04 -0500

    02262026 Updates

--- Aqualog_PARAFAC/Abs_Analysis_Functions.R
+++ Aqualog_PARAFAC/Abs_Analysis_Functions.R
@@ -9,8 +9,45 @@ names(data_abs) <- sub("\\.dat$", "", basename(file_abs))
 
 for (i in 1:length(data_abs)){
   colnames(data_abs[[i]]) <- c("wavelength", "abs")
+  
+  ##correct all negative values
+  data_abs[[i]]$abs[data_abs[[i]]$abs < 0] <- 0
 }
 
 return(data_abs)
 
+}
+
+blank_subtract <- function(blank, data){
+  
+  for(i in 1:length(data)){
+    
+    for (ii in 1:nrow(data[[i]])){
+      
+      data[[i]]$abs[ii] <- data[[i]]$abs[ii] -data[[blank]]$abs[ii]
+      
+    }
+  }
+  
+  data <- data[-blank]
+  
+  return(data)
+  
+}
+
+abs_parm_plus <- function(data){
+  
+  slope_parm <- list()
+  
+  for (i in 1:length(data)){
+    
+    slope_parm[[i]] <- abs_parms(data[[i]], cuvl = 1)
+  
+  }
+  
+  slope_parm <- data.frame(do.call(rbind, slope_parm))
+  
+  slope_parm$sample <- names(data)
+  
+  return(slope_parm)
 }
