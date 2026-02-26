97e58e3025be3c947649f4cc3d881c85f4c86209
Tia Ouyang <touyang@umces.edu>
2026-02-26 11:04:17 -0500

    02262026 Updates

---
+++ Aqualog_PARAFAC/EEMs_Analysis_Functions.R
@@ -0,0 +1,34 @@
+eem_corr <- function(data, blank){
+  
+  for(i in 1:length(data)){
+    
+    data[[i]][["x"]] = data[[i]][["x"]] - data[[blank]][["x"]]
+    
+    data[[i]][["x"]][data[[i]][["x"]] < 0] <- 0
+    
+  }
+  
+  data <- data[-blank]
+  
+  # restore eemlist class (critical)
+  class(data) <- unique(c("eemlist", class(data)))
+  
+  return(data)
+  
+}
+
+
+eem_indice <- function(data, output_name){
+  
+  bix <- eem_biological_index(data)
+  coble_peaks <- eem_coble_peaks(data)
+  fi <- eem_fluorescence_index(data)
+  hix <- eem_humification_index(data)
+  output_name <- bix %>%
+    full_join(coble_peaks, by = "sample") %>%
+    full_join(fi, by = "sample") %>%
+    full_join(hix, by = "sample")
+  
+  return(output_name)
+  
+}
