ae9e595595eb8369a0a33be9d350d2861c838698
Tia Ouyang <touyang@umces.edu>
2026-04-27 12:05:48 -0400

    04272026 Updates

--- Shimadzu_TOC/Shimadzu_TOC_Functions.R
+++ Shimadzu_TOC/Shimadzu_TOC_Functions.R
@@ -16,5 +16,7 @@ plot_cal <- function(data, fit){
   
   eq <- paste0("y = ",round(coef(fit)[2],2), " x + ",round(coef(fit)[1],2))
   
-  ggplot(data,aes(x = std_conc, y = Area)) + geom_point(size = 3) + geom_smooth(method = "lm", linetype = "dashed", linewidth = 1.5, color = "lightblue3") + theme_bw() + theme(panel.grid = element_line(color = "gray", linewidth = 0.2, linetype = "dashed"), axis.title = element_text(size = 12, face = "bold"), axis.text = element_text(size = 12)) + ylab("Instrumental Signals") + annotate(geom = "text", label = eq, x = -Inf, y = Inf, hjust = -0.1, vjust = 2,fontface = "bold",size = 4.5, color = "navy")
+  p <- ggplot(data,aes(x = std_conc, y = Area)) + geom_point(size = 3) + geom_smooth(method = "lm", linetype = "dashed", linewidth = 1.5, color = "lightblue3") + theme_bw() + theme(panel.grid = element_line(color = "gray", linewidth = 0.2, linetype = "dashed"), axis.title = element_text(size = 12, face = "bold"), axis.text = element_text(size = 12)) + ylab("Instrumental Signals") + annotate(geom = "text", label = eq, x = -Inf, y = Inf, hjust = -0.1, vjust = 2,fontface = "bold",size = 4.5, color = "navy")
+  
+  return(p)
 }
